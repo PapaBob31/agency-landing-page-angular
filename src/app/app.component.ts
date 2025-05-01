37b0002fca4ae4a21cb86f8ba0a57266008dce86
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-05-01 20:08:31 +0100

    Add code to create working site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'agency-landing-page-angular';
+  menuIsShown = false
+  menu: HTMLElement;
+  menuBtn: HTMLButtonElement;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.document.addEventListener("click", this.toggleMenuVisibility)
+    this.menu = this.document.querySelector("nav")!;
+    this.menuBtn = this.document.querySelector("menu-btn")!
+    this.menuIsShown = false;
+  }
+
+  toggleMenuVisibility(e: MouseEvent): void {
+    if (this.menuIsShown) {
+      this.menu.classList.remove("shown")
+      this.menuIsShown = false;
+    }else{
+      if (e.target !== this.menuBtn) {
+        return; // only menuBtn should open the menu when clicked
+      }
+      this.menu.classList.add("shown")
+      this.menuIsShown = true;
+    }
+  }
 }
