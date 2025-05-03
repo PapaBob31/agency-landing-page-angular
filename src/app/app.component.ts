cda77d59f4023cddf9c5af36d3cdaee565bf2421
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-05-03 11:34:48 +0100

    Add menu display click handler on small screen widths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, Inject } from '@angular/core';
-import { DOCUMENT } from "@angular/common";
+import { Component, inject, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,28 +6,17 @@ import { DOCUMENT } from "@angular/common";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  menuIsShown = false
-  menu: HTMLElement;
-  menuBtn: HTMLButtonElement;
 
-  constructor(@Inject(DOCUMENT) private document: Document) {
-    this.document.addEventListener("click", this.toggleMenuVisibility)
-    this.menu = this.document.querySelector("nav")!;
-    this.menuBtn = this.document.querySelector("menu-btn")!
-    this.menuIsShown = false;
+export class AppComponent {
+  menuIsShown: boolean = false; // controls the visisbility of the menu on mobile screens
+
+  constructor() {
+    const elementRef = inject(ElementRef);
+    elementRef.nativeElement.addEventListener("click", ()=>{this.menuIsShown = false})
   }
 
-  toggleMenuVisibility(e: MouseEvent): void {
-    if (this.menuIsShown) {
-      this.menu.classList.remove("shown")
-      this.menuIsShown = false;
-    }else{
-      if (e.target !== this.menuBtn) {
-        return; // only menuBtn should open the menu when clicked
-      }
-      this.menu.classList.add("shown")
-      this.menuIsShown = true;
-    }
+  toggleMenuVisibility(event: MouseEvent): void {
+    event.stopPropagation()
+    this.menuIsShown = !this.menuIsShown;
   }
 }
